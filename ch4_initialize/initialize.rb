10c936b7c2b0b8e25d22188c7643181f0edccc8a
Robert Laws <roblaws@me.com>
2017-04-26 16:01:08 +0300

    more initialize

---
+++ ch4_initialize/initialize.rb
@@ -0,0 +1,41 @@
+# usage of the initialize method
+
+# class MyClass
+#   def initialize
+#     puts "setting up"
+#   end
+# end
+#
+# item = MyClass.new
+
+class Employee
+  attr_reader :name, :salary
+
+  def name=(value)
+    if value == ""
+      raise "Name cannot be blank"
+    end
+    @name = value
+  end
+
+  def salary=(value)
+    if value < 0
+      raise "Salary of #{value} is not valid"
+    end
+  end
+
+  def initialize(name = "Unknown", salary = 0.0)
+    @name = name
+    @salary = salary
+  end
+
+  def print_pay_stub
+    puts "Name: #{@name}"
+    pay_for_period = (@salary / 365.0) * 14
+    formatted_pay = format("$%0.2f", pay_for_period)
+    puts "Pay: #{formatted_pay}"
+  end
+end
+
+emp = Employee.new("Kal", 40000)
+emp.print_pay_stub
